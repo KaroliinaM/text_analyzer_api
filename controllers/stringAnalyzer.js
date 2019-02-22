06cde47d001ec6f7977fceb910f318c6c9d5427d
Your Name <karoliina.mietola@helsinki.fi>
2019-02-22 17:58:02 +0200

    refactor api structure

--- controllers/stringAnalyzer.js
+++ controllers/stringAnalyzer.js
@@ -1,13 +1,19 @@
 const apiRouter=require('express').Router()
 const WordAnalyzer=require('../components/apiResponse')
 
-apiRouter.post('/', (req, res) => {
-  const sentence=req.body
-  if(sentence.text === undefined) {
-    return res.status(400).json({ error: 'bad request' })
+apiRouter.post('/', async (req, res) => {
+  try
+  {
+    const sentence=req.body
+    if(sentence.text === undefined) {
+      return res.status(400).json({ error: 'bad request' })
+    }
+    const result=await WordAnalyzer.collect(sentence.text)
+    res.json(result)
+  } catch(exception) {
+    console.log(exception)
+    res.status(500).json({error: 'error: something wrong'})
   }
-  const result=WordAnalyzer.collect(sentence.text)
-  res.json(result)
 })
 
 module.exports=apiRouter
--- index.js
+++ index.js
@@ -5,27 +5,16 @@ const cors=require('cors')
 const app=express()
 const apiRouter=require('./controllers/stringAnalyzer')
 const config=require('./utils/config')
-//const WordAnalyzer=require('./components/apiResponse')
 
 app.use(bodyparser.json())
 app.use('/analyze', apiRouter)
 app.use(cors())
 
-// app.post('/analyze', (req, res) => {
-//   const sentence=req.body
-//   if(sentence.text === undefined) {
-//     return res.status(400).json({ error: 'bad request' })
-//   }
-//   const result=WordAnalyzer.collect(sentence.text)
-//   res.json(result)
-// })
 app.get('/test', (req, res) => {
   res.send('<h1>Hello Wold</h1>')
 })
 
 const server=http.createServer(app)
-
-//const PORT=process.env.PORT || 3001
 
 server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
